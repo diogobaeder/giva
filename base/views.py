3e746841c42ac06b9cf7d4b1f7d1b0f1d8e52b44
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2014-08-24 03:02:40 -0300

    Ordenando botões das páginas de programa de acordo com a ordem definida no admin

--- base/views.py
+++ base/views.py
@@ -4,5 +4,6 @@ from mezzanine.pages.models import Page
 
 
 def home(request):
-    programme = Page.objects.filter(parent__titles='Programa')
+    programme = Page.objects.filter(parent__titles='Programa').order_by(
+        '_order')
     return render(request, 'index.html', {'programme': programme})
